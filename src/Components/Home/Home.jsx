8b6e33990699560321d107f4a12722f2958e6600
Amaam_Rizvi <128987283+AmaamRizvi@users.noreply.github.com>
2024-04-06 09:31:53 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,8 +9,8 @@ import { Link } from "react-router-dom";
 export default function Home() {
   const imageLinks = [
     "https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg",
-    "https://img.freepik.com/premium-photo/colorful-sport-shoes-green_151013-4554.jpg",
-    "https://media.istockphoto.com/id/1320501530/photo/white-sneaker-on-a-blue-gradient-background-mens-fashion-sport-shoe-sneakers-lifestyle.jpg?b=1&s=612x612&w=0&k=20&c=tAWSqAFCbYz1wLQuUTYoCK13DFIoML7yd-yl_TtwWtI=",
+    "https://assets.adidas.com/images/w_600,f_auto,q_auto/c23bd1066d7c4e07a35dadb200ebdde8_9366/CORE_LINEAR_STUNICON_SHOES_Blue_EY2984_01_standard.jpg",
+    "https://di2ponv0v5otw.cloudfront.net/posts/2023/07/04/64a4ff47e86380b18bef92d1/s_64a4ff497dfcc26c4216d374.jpeg,
     "https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_640.jpg",
     "https://t4.ftcdn.net/jpg/05/06/36/71/360_F_506367145_aTN8tLqtKXDYxzHQs5DH4HGsbVT9OgMn.jpg",
   ];
